733556700fb3f1ddfdb543ae145a76c513cdf036
Trudy <trudy@vpnhood.com>
2025-01-04 14:14:53 -0800

    Update project files and enhance Android version checks

--- DotNet/VhInMobi/InMobiUtil.cs
+++ DotNet/VhInMobi/InMobiUtil.cs
@@ -1,5 +1,4 @@
-﻿using Android.Runtime;
-using Com.Vpnhood.Inmobi.Ads;
+﻿using Com.Vpnhood.Inmobi.Ads;
 using VpnHood.Core.Common.Utils;
 using VpnHood.Core.Client.Device.Droid.Utils;
 
@@ -9,23 +8,30 @@ public class InMobiUtil
 {
     private static readonly AsyncLock InitLock = new();
     public static bool IsInitialized { get; private set; }
+    public static int RequiredAndroidVersion => 28;
+    public static bool IsAndroidVersionSupported => OperatingSystem.IsAndroidVersionAtLeast(RequiredAndroidVersion);
 
-    public static async Task Initialize(Activity activity, string accountId, bool isDebugMode,
+    public static async Task Initialize(Activity activity, string accountId, bool isDebugMode, TimeSpan timeout,
         CancellationToken cancellationToken)
     {
+        // it looks like there is conflict between InMobi SDK and Xamarin.GooglePlayServices.Ads.Identifier 118.2.0
+        // However we have decided to stop testing InMobi SDK in any android lower than 9.0
+        if (!IsAndroidVersionSupported)
+            throw new NotSupportedException("InMobi SDK requires Android 9.0 or higher.");
+
         using var lockAsync = await InitLock.LockAsync(cancellationToken);
         if (IsInitialized)
             return;
 
         // initialize
-        var initTask = await AndroidUtil.RunOnUiThread(activity, () => InMobiAdServiceFactory.InitializeInMobi(activity, accountId,
-                Java.Lang.Boolean.ValueOf(isDebugMode))!.AsTask())
+        var initTask = await AndroidUtil.RunOnUiThread(activity,
+                () => InMobiAdServiceFactory.InitializeInMobi(activity, accountId, Java.Lang.Boolean.ValueOf(isDebugMode))!.AsTask())
             .WaitAsync(cancellationToken)
             .ConfigureAwait(false);
 
         // wait for completion
         await initTask
-            .WaitAsync(cancellationToken)
+            .WaitAsync(timeout, cancellationToken)
             .ConfigureAwait(false);
 
         IsInitialized = true;
