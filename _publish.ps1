62f0555c0a799824b684e7500874005e33b64c3a
Trudy <trudy@vpnhood.com>
2025-03-03 17:35:24 -0800

    Publish

--- _publish.ps1
+++ _publish.ps1
@@ -4,6 +4,3 @@ $gitDir = "$solutionDir/.git";
 git --git-dir=$gitDir --work-tree=$solutionDir pull;
 git --git-dir=$gitDir --work-tree=$solutionDir commit -a -m "Publish";
 git --git-dir=$gitDir --work-tree=$solutionDir push;
-
-# swtich to main branch
-git --git-dir=$gitDir push origin Development:main
